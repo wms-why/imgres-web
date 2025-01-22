8d71697ca43f2c77bca62188428330c9ee7f813f
ymk <mkyms@outlook.com>
2025-01-22 10:23:26 +0800

    添加sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_DOMAIN!;
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/price`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: "2025-01-22T02:22:06.416Z",
+    },
+    {
+      url: `${baseUrl}/refund-policy`,
+      lastModified: "2025-01-22T02:22:06.416Z",
+    },
+    {
+      url: `${baseUrl}/terms-of-service`,
+      lastModified: "2025-01-22T02:22:06.416Z",
+    },
+  ];
+}
